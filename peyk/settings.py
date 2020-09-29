c1c503813e3a130bb412b58e770416279ff6437b
Ali Mahdiyar <alimahdiyar77@gmail.com>
2020-09-29 16:38:08 +0330

    rest framework token authentication

--- peyk/settings.py
+++ peyk/settings.py
@@ -76,6 +76,13 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
